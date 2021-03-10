bfcabb0b00cff46d101563ee062b2a8c99e34b57
jordi.peralta <jordi.peraltadolz@ingrammicro.com>
2021-03-10 18:12:31 +0100

    Add CSV renderer

--- connect/reports/renderers/__init__.py
+++ connect/reports/renderers/__init__.py
@@ -7,3 +7,4 @@ from connect.reports.renderers.j2 import Jinja2Renderer # noqa
 from connect.reports.renderers.json import JSONRenderer # noqa
 from connect.reports.renderers.pdf import PDFRenderer # noqa
 from connect.reports.renderers.xlsx import XLSXRenderer # noqa
+from connect.reports.renderers.csv import CSVRenderer # noqa
