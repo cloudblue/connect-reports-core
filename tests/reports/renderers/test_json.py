540af48a85efbf76b34598f3b64826d66a9817c4
jordi.peralta <jordi.peraltadolz@ingrammicro.com>
2021-03-17 13:42:39 +0100

    LITE-17784: Add realistic tests using tempfs

--- tests/reports/renderers/test_json.py
+++ tests/reports/renderers/test_json.py
@@ -1,6 +1,10 @@
 from connect.reports.datamodels import RendererDefinition
 from connect.reports.renderers import JSONRenderer
 
+from fs.tempfs import TempFS
+
+import json
+
 
 def test_validate_ok():
 
@@ -33,3 +37,18 @@ def test_render(mocker, account_factory, report_factory, report_data):
     assert renderer.render(data, 'report') == 'report.json'
     assert mocked_dump.mock_calls[0].args[0] == data
     assert mocked_open.mock_calls[0].args == ('report.json', 'w')
+
+
+def test_render_tmpfs_ok(account_factory, report_factory, report_data):
+    tmp_fs = TempFS()
+    data = report_data(2, 2)
+    renderer = JSONRenderer(
+        'runtime',
+        tmp_fs.root_path,
+        account_factory(),
+        report_factory(),
+    )
+    output_file = renderer.render(data, f'{tmp_fs.root_path}/report')
+
+    assert output_file == f'{tmp_fs.root_path}/report.json'
+    assert open(output_file).read() == json.dumps(data)
